4f78eddafc6fd2de2ef5ea1d8a956f0e73088e16
jona62 <jonathanjamesm66@gmail.com>
2023-07-01 17:55:27 -0400

    Various Dependency upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,15 +19,15 @@ repositories {
 }
 
 dependencies {
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	runtimeOnly("org.postgresql:postgresql")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.20-RC")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc:3.0.4")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.4")
+	implementation("org.springframework.boot:spring-boot-starter-validation:3.0.4")
+	runtimeOnly("org.postgresql:postgresql:42.5.4")
 	// Testing
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
 	testImplementation("com.ninja-squad:springmockk:4.0.2")
 	testImplementation("io.mockk:mockk:1.13.5")
 }
